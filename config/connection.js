c3bed0904b2f3d54aa1a97115f180329a562087a
Preston Carey <pcarey1213@gmail.com>
2019-02-28 00:08:02 -0500

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 var mysql= require ("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else{
+  connection = mysql.createConnection({
   host:"localhost",
   user: "root",
   port: 3306,
   password:"Hamill09",
   database:"burgers_db"
 });
+}
 
 connection.connect(function(err){
   if (err){
